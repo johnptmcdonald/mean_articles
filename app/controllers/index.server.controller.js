f5b3dd1e0414fe1eff3256589af43c61fb97e36a
johnptmcdonald <johnptmcdonald@gmail.com>
2016-04-14 07:46:55 -0400

    about to add express-session

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,3 +1,5 @@
 module.exports.render = function(req, res){
-	res.send('Hello World')
+	res.render('index',{
+		title: 'HelloWorld'
+	})
 }
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
+// the config/express file returns a function
 var express = require('./config/express')
 
+// which we then call to get the app that we have configured
 var app = express()
+
 app.listen(3000)
 
 module.exports = app;
