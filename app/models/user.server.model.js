fecef0a8d11aaf3c2cdd0c1aa73a92c9645d5d31
johnptmcdonald <johnptmcdonald@gmail.com>
2016-04-14 11:13:06 -0400

    adds User model validation, adds Post model

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -5,9 +5,60 @@ var Schema = mongoose.Schema;
 var UserSchema = new Schema({
 	firstName: String,
 	lastName: String,
-	email: String,
-	username: String,
-	password: String
+	email: {
+		type: String,
+		index: true,
+		match: /.+\@.+\..+/
+	},
+	username: {
+		type: String,
+		trim: true,
+		unique: true,
+		required: true
+	},
+	password: {
+		type: String,
+		validate: [
+			function(password){
+				return password.length >= 6
+			}, "Password must be longer than 5 characters"
+		]
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	website: {
+		type: String, 
+		get: function(url){
+			if(!url){
+				return url
+			} else {
+				if(url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0){
+					url = 'http://' + url
+				} 
+				return url
+			}
+		}
+	}
 })
+
+UserSchema.post('save', function(next){
+	if(this.isNew){
+		console.log("A new user was created")
+	} else {
+		console.log("A user updated their details")
+	}
+})
+
+UserSchema.statics.findOneByUsername = function(username, callback){
+	this.findOne({username: new RegExp(username, 'i')}, callback)
+}
+
+UserSchema.virtual('fullName').get(function(){
+	return this.firstName + " " + this.lastName;
+})
+
+UserSchema.set('toJSON', {getters: true, virtuals: true})
 
 mongoose.model('User', UserSchema)
